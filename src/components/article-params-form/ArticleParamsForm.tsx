9bd3642149e24e31d17378612e2c27a1f64502a2
l0nik <l0nik@mail.ru>
2025-04-17 20:23:33 +0300

    открытие/закрытие сайдбара

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,27 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState, useRef, useEffect } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const articleParamsRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		articleParamsRef.current!.className = isOpen
+			? `${styles.container} ${styles.container_open}`
+			: styles.container;
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
+			/>
+			<aside className={styles.container} ref={articleParamsRef}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
