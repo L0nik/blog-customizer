37edf616c83c3179819c6c2b33c4d385637ebc85
l0nik <l0nik@mail.ru>
2025-04-18 18:13:22 +0300

    В сайдбар выведены компоненты для настройки параметров страницы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,10 +1,28 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 import { useState, useRef, useEffect } from 'react';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	fontSizeOptions,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+};
+
+export const ArticleParamsForm = ({ articleState }: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const articleParamsRef = useRef<HTMLElement | null>(null);
 
@@ -13,6 +31,41 @@ export const ArticleParamsForm = () => {
 			? `${styles.container} ${styles.container_open}`
 			: styles.container;
 	});
+
+	const [fontFamily, setFontFamily] = useState<OptionType>(
+		articleState.fontFamilyOption
+	);
+	const handleFontFamilyChange = (selected: OptionType) => {
+		setFontFamily(selected);
+	};
+
+	const [fontSize, setFontSize] = useState<OptionType>(
+		articleState.fontSizeOption
+	);
+	const handleFontSizeChange = (selected: OptionType) => {
+		setFontSize(selected);
+	};
+
+	const [fontColor, setFontColor] = useState<OptionType>(
+		articleState.fontColor
+	);
+	const handleFontColorChange = (selected: OptionType) => {
+		setFontColor(selected);
+	};
+
+	const [backgroundColor, setBackgroundColor] = useState<OptionType>(
+		articleState.backgroundColor
+	);
+	const handleBackgroundColorChange = (selected: OptionType) => {
+		setBackgroundColor(selected);
+	};
+
+	const [contentWidth, setContentWidth] = useState<OptionType>(
+		articleState.contentWidth
+	);
+	const handleContentWidthChange = (selected: OptionType) => {
+		setContentWidth(selected);
+	};
 
 	return (
 		<>
@@ -24,6 +77,41 @@ export const ArticleParamsForm = () => {
 			/>
 			<aside className={styles.container} ref={articleParamsRef}>
 				<form className={styles.form}>
+					<Text size={31} weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='шрифт'
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={handleFontFamilyChange}
+					/>
+					<RadioGroup
+						title='размер шрифта'
+						name='fontSizeRadioGroup'
+						selected={fontSize}
+						options={fontSizeOptions}
+						onChange={handleFontSizeChange}
+					/>
+					<Select
+						title='цвет шрифта'
+						selected={fontColor}
+						options={fontColors}
+						onChange={handleFontColorChange}
+					/>
+					<Separator />
+					<Select
+						title='цвет фона'
+						selected={backgroundColor}
+						options={backgroundColors}
+						onChange={handleBackgroundColorChange}
+					/>
+					<Select
+						title='ширина контента'
+						selected={contentWidth}
+						options={contentWidthArr}
+						onChange={handleContentWidthChange}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
